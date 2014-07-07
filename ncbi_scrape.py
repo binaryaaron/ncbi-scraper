ffa86254ab7fc74b0cca4075d4816e3776eaffdb
Aaron Gonzales <xysmas@gmail.com>
2014-07-07 10:07:50 -0600

    added docstrings/comments to ncbi_scrape.py

--- ncbi_scrape.py
+++ ncbi_scrape.py
@@ -7,20 +7,37 @@ import re
 import collections
 
 
+
 def ncbiUrlBuilder(snp):
+""" ncbiUrlBuilder builds the url used by NCBI to query a particular SNP. 
+an example url for snp rs96066708 is below:
+http://www.ncbi.nlm.nih.gov/projects/SNP/snp_ref.cgi?rs=9606708
+and will give the information page for that SNP. It builds the URL based on
+the base URL from the ncbi site and concatenates it with some simple string
+building tools. 
+The URL is passed to mechanize/Browser to open the site, and the opened page
+is returned to the program.
+Arguments:
+snp -- the snp id. 
+"""
     #url = "http://www.ncbi.nlm.nih.gov/projects/SNP/snp_ref.cgi?rs=9606708"
     print 'attempting to query', snp
     snpNumber = snp[2:(len(snp))]
-    #url = "http://www.ncbi.nlm.nih.gov/projects/SNP/snp_ref.cgi?rs=9606708"
     url = "http://www.ncbi.nlm.nih.gov/projects/SNP/snp_ref.cgi?" + "rs=" + snpNumber
     print 'trying url: ' + url 
     mech = Browser()
     page = mech.open(url)
     return page, snp
 
-
-
 def tablechopper(pageinfo):
+"""tablechopper is the primary workhorse of the program. 
+It uses BeautifulSoup to parse the page opened by ncbiUrlBuilder 
+and then adds the contents of the Allele table to a list (finalList),
+which is returned to the program.
+Arguments:
+    pageinfo -- list that contains the ncbiUrlBuilder page and the
+                snp number.
+"""
     soup = BeautifulSoup(pageinfo[0])
     table = soup.find(id="Allele")
     rows = table.contents
